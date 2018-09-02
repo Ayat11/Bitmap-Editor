b5f1b09248874b90b3178dcb59381450358168b7
Ayat11 <ayatafy@gmail.com>
2018-09-02 18:21:39 +0200

    added colorize gem

--- lib/image.rb
+++ lib/image.rb
@@ -10,18 +10,20 @@ class Image
   end
 
   def color_pixels(row, col, color)
-    @bitmap[row][col] = color
+    @bitmap[row][col] = color.colorize(random_colorize)
   end
 
   def draw_vertical_segment(row_1, row_2, col, color)
+    random_color = random_colorize
     (row_1..row_2).each do |row|
-      @bitmap[row][col] = color
+      @bitmap[row][col] = color.colorize(random_color)
     end
   end
 
   def draw_horizontal_segment(col_1, col_2, row, color)
+    random_color = random_colorize
     (col_1..col_2).each do |col|
-      @bitmap[row][col] = color
+      @bitmap[row][col] = color.colorize(random_color)
     end
   end
 
@@ -31,5 +33,11 @@ class Image
 
   def clear
     @bitmap = Array.new(@rows) { Array.new(@cols, WHITE) }
+  end
+
+  private
+
+  def random_colorize
+    [:red, :green, :yellow, :blue, :magenta, :cyan][rand(0..5)]
   end
 end
